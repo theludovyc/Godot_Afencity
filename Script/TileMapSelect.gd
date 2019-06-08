9af8e36fb7d33628aaa3c465fa6544f71f56a627
unknown <theludovyc@gmail.com>
2019-06-08 15:41:20 +0200

    selection OK

--- Script/TileMapSelect.gd
+++ Script/TileMapSelect.gd
@@ -8,6 +8,29 @@ var mouseBool = false
 var select_start:Vector2
 var select_tmp:Vector2
 
+func fill_select(pos_ori, pos_fina, tile_index):
+	var diff_x = abs(pos_fina.x-pos_ori.x)
+	var diff_y = abs(pos_fina.y-pos_ori.y)
+	
+	if pos_fina.x < pos_ori.x:
+		pos_fina.x+=diff_x
+		pos_ori.x-=diff_x
+		
+	if pos_fina.y < pos_ori.y:
+		pos_fina.y+=diff_y
+		pos_ori.y-=diff_y
+	
+	if diff_y == 0 and diff_x>0:
+		for i in range(diff_x+1):
+			set_cell(pos_ori.x+i, pos_ori.y, tile_index)
+	elif diff_x == 0:
+		for j in range(diff_y+1):
+			set_cell(pos_ori.x, pos_ori.y+j, tile_index)
+	else:
+		for j in range(diff_y+1):
+			for i in range(diff_x+1):
+				set_cell(pos_ori.x+i, pos_ori.y+j, tile_index)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,19 +40,15 @@ func _process(delta):
 	if Input.is_action_just_pressed("mouse_left_button"):
 		select_start = world_to_map(get_global_mouse_position())
 		select_tmp = select_start
-		set_cell(select_start.x, select_start.y, 3)
 		mouseBool=true
 	
 	if mouseBool:
 		var v = world_to_map(get_global_mouse_position())
 		if v != select_tmp:
-			for i in range(select_tmp.x-select_start.x):
-				set_cell(select_start.x+i,select_start.y,-1)
+			fill_select(select_start, select_tmp, -1)
 			select_tmp = v
-			for i in range(select_tmp.x-select_start.x):
-				set_cell(select_start.x+i,select_start.y,3)
+			fill_select(select_start, select_tmp, 3)
 	
 	if Input.is_action_just_released("mouse_left_button"):
-		set_cell(select_start.x, select_start.y, -1)
 		mouseBool=false
 	pass
