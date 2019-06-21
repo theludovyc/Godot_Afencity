ed56554b3f369a777da2d7607ade686931363e54
unknown <theludovyc@gmail.com>
2019-06-21 14:26:58 +0200

    update selection

--- Script/TileMap.gd
+++ Script/TileMap.gd
@@ -85,7 +85,7 @@ func reset_selected_cells(pos_ori, pos_fina):
 			for i in range(diff_x+1):
 				reset_cell(pos_ori.x+i, pos_ori.y+j)
 
-func fill_selected_cells(pos_ori, pos_fina, tile_index):
+func set_selected_cells(pos_ori, pos_fina, tile_index):
 	var diff_x = abs(pos_fina.x-pos_ori.x)
 	var diff_y = abs(pos_fina.y-pos_ori.y)
 	
@@ -108,24 +108,31 @@ func fill_selected_cells(pos_ori, pos_fina, tile_index):
 			for i in range(diff_x+1):
 				set_cell(pos_ori.x+i, pos_ori.y+j, tile_index)
 
-func reset_fill_selected_cells(pos_ori, old_pos_fina, new_pos_fina, tile_index):
-	var diff_x_old = abs(old_pos_fina.x-pos_ori.x)
-	var diff_x_new = abs(new_pos_fina.x-pos_ori.x)
+func save_cell(x, y, tile_index):
+	array[y*64+x]=tile_index
 
-	if diff_x_old < diff_x_new:
-		#fill_x
-	else:
-		#reset_x
-
-	var diff_y_old = abs(old_pos_fina.y-pos_ori.y)
-	var diff_y_new = abs(new_pos_fina.y-pos_ori.y)
-
-	if diff_y_old < diff_y_new:
-		#fill_y
-	else:
-		#reset_x
-
+func save_selected_cells(pos_ori, pos_fina, tile_index):
+	var diff_x = abs(pos_fina.x-pos_ori.x)
+	var diff_y = abs(pos_fina.y-pos_ori.y)
 	
+	if pos_fina.x < pos_ori.x:
+		pos_fina.x+=diff_x
+		pos_ori.x-=diff_x
+		
+	if pos_fina.y < pos_ori.y:
+		pos_fina.y+=diff_y
+		pos_ori.y-=diff_y
+	
+	if diff_y == 0 and diff_x>0:
+		for i in range(diff_x+1):
+			save_cell(pos_ori.x+i, pos_ori.y, tile_index)
+	elif diff_x == 0:
+		for j in range(diff_y+1):
+			save_cell(pos_ori.x, pos_ori.y+j, tile_index)
+	else:
+		for j in range(diff_y+1):
+			for i in range(diff_x+1):
+				save_cell(pos_ori.x+i, pos_ori.y+j, tile_index)
 
 func _process(delta):
 	if Input.is_action_just_pressed("mouse_left_button"):
@@ -138,9 +145,10 @@ func _process(delta):
 		if v != select_tmp:
 			reset_selected_cells(select_start, select_tmp)
 			select_tmp = v
-			fill_selected_cells(select_start, select_tmp, 3)
+			set_selected_cells(select_start, select_tmp, 3)
 	
 	if Input.is_action_just_released("mouse_left_button"):
+		save_selected_cells(select_start, select_tmp, 3)
 		mouseBool=false
 	pass
 
